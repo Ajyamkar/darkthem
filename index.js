534dbadf350455d3ab93e9bdc4e946b239a9ee5f
Ajay Yamkar <58873896+Ajyamkar@users.noreply.github.com>
2020-09-23 18:08:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+document.querySelector("input").addEventListener("click",function(){
+  document.querySelector("body").classList.toggle("bodybg");
+  document.querySelector("div").classList.toggle("colorBg");
+  // var background = document.querySelector(".color").style.backgroundColor;
+  // var n=1;
+  // while(n<=10){
+  //   if(background == "")
+  //   {
+  //   document.querySelector(".color").style.backgroundColor = "rgb(26,255,0)";
+  //   }else{
+  //       document.querySelector(".color").style.backgroundColor = "rgb(255,145,0)";
+  //     }
+  //       n++;
+  // }
+
+});
